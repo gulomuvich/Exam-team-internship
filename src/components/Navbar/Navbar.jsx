59450033710f28730f09ea3bbe90570a74f2d6f5
xabibnurmagemedov856-cmd <xabibnurmagemedov856@gmail.com>
2026-07-14 15:30:40 +0500

    ggg

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
 import "./Navbar.css";
 
 const Navbar = () => {
@@ -10,7 +11,7 @@ const Navbar = () => {
             <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
             <polyline points="9 22 9 12 15 12 15 22"></polyline>
           </svg>
-          <Link className="nav-text" to={}>Dashboard</Link>
+          <Link className="nav-text" to={<Dashboard/>}>Dashboard</Link>
         </li>
 
         <li>
