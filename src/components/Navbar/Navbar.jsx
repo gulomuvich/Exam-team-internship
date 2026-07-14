eb4958571c0604ca86680bd0f4992f952166c026
xabibnurmagemedov856-cmd <xabibnurmagemedov856@gmail.com>
2026-07-14 15:31:13 +0500

    gggg

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
             <polyline points="9 22 9 12 15 12 15 22"></polyline>
           </svg>
-          <Link className="nav-text" to={<Dashboard/>}>Dashboard</Link>
+          <Link className="nav-text" to={"Dashboard"}>Dashboard</Link>
         </li>
 
         <li>
