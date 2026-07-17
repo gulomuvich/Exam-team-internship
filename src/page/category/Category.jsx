e62171e6eb59b81106e612e803bcbd04f1880bf2
Abubakr013 <mpro39801@gmail.com>
2026-07-17 16:13:48 +0500

    category complated

---
+++ src/page/category/Category.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import "./category.css";
+
+function Category() {
+  const [showModal, setShowModal] = useState(false);
+
+  const [categories, setCategories] = useState([]);
+
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
+
+  function addCategories() {
+    const newCategory = {
+      id: Date.now(),
+      name,
+      image,
+      item: 0,
+    };
+
+    setCategories([...categories, newCategory]);
+
+    setName("");
+    setImage("");
+
+    setShowModal(false)
+  }
+
+  return (
+    <div className="categories">
+      <div className="top">
+        <h2>Categories</h2>
+
+        <button className="addBtn" onClick={() => setShowModal(true)}>
+          + Add Category
+        </button>
+      </div>
+
+      <div className="cards">
+        {categories.map((item) => (
+          <div className="card" key={item.id}>
+            <img src={item.image} alt="" />
+
+            <div className="cardBody">
+              <h3>{item.name}</h3>
+              <p>{item.items} items</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {showModal && (
+        <div className="modal">
+          <div className="modalBox">
+            <button className="close" onClick={() => setShowModal(false)}>
+              ×
+            </button>
+
+            <h2>Add Category</h2>
+
+            <label>Category Name</label>
+            <input
+              type="text"
+              placeholder="Category Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
+            <label>Category Image URL</label>
+            <input
+              type="text"
+              placeholder="Image URL"
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
+            />
+
+            <div className="buttons">
+              <button className="cancel" onClick={() => setShowModal(false)}>
+                Cancel
+              </button>
+
+              <button className="create" onClick={addCategories}>
+                Create Category
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Category;
