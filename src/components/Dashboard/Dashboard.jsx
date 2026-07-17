c0d6cb8a6a94b9bbff3f899bfb0796b072b0eab3
xabibnurmagemedov856-cmd <xabibnurmagemedov856@gmail.com>
2026-07-17 15:45:34 +0500

    xato

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,77 @@
 import "./Dashboard.css";
+import icon from "../../assets/icon/icon.png"
+import market from "../../assets/icon/icon (1).png"
+import puple from "../../assets/icon/icon (2).png"
+import add from "../../assets/icon/icon (3).png"
+import Dashbor from "../../assets/icon/Orders chart (1).png"
+import last from "../../assets/icon/last 7 days.png"
+import { Link } from "react-router-dom";
+import prodo from "../../assets/icon/Top Products.png"
 
 const Dashboard = () => {
-
+return (
+  <section>
+    <ul className="doshbord-left">
+      <li className="dosbord-list">
+        <div>
+          <h2 className="dashbord-title">
+            $10.540
+          </h2>
+          <p className="deshbord-text">
+            Total Revenue
+          </p>
+          <mark className="deshbord-mark">22.45% </mark>
+        </div>
+        <img src={icon} alt="" />
+      </li>
+       <li className="dosbord-list">
+        <div>
+          <h2>
+            $10.540
+          </h2>
+          <p>
+            Total Revenue
+          </p>
+          <mark className="deshbord-mark">22.45% </mark>
+        </div>
+        <img src={market} alt="" />
+      </li>
+       <li className="dosbord-list">
+        <div>
+          <h2>
+            $10.540
+          </h2>
+          <p>
+            Total Revenue
+          </p>
+          <mark>22.45% </mark>
+        </div>
+        <img src={puple} alt="" />
+      </li>
+       <li className="dosbord-list">
+        <div>
+          <h2>
+            $10.540
+          </h2>
+          <p>
+            Total Revenue
+          </p>
+          <mark className="">22.45% </mark>
+        </div>
+        <img src={add} alt="" />
+      </li>
+    </ul>
+    <div className="last-img">
+      <img src={Dashbor} alt="" />
+      <img src={last} alt="" />
+    </div>
+    <div className="prodo-img">
+      <Link to={"/products"} >
+      <img src={prodo} alt="" />
+      </Link>
+    </div>
+  </section>
+)
 };
 
 export default Dashboard;
