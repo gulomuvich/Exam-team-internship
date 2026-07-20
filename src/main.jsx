4ed6528cf687ca16c54e479695edacd4a9f42b5d
xabibnurmagemedov856-cmd <xabibnurmagemedov856@gmail.com>
2026-07-20 17:48:12 +0500

    redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
-
-    <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Provider>
       <App />
-    </BrowserRouter>
-)
+    </Provider>
+  </BrowserRouter>,
+);
